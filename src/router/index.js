9033f1c332834c5490cc95dc235314dea9ff88e7
Mateusz <wirus006@gmail.com>
2020-08-30 13:02:10 +0200

    basic app view

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,9 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component() {
+      return import(/* webpackChunkName: "about" */ '../views/About.vue');
+    },
   },
 ];
 
